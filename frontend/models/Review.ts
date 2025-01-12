777d9603dd5ed750e8678cdc47db1251eb7f3990
Hemang Patel <hemang9705@gmail.com>
2025-01-12 17:59:11 +0530

    added review,whishlist,order and multiple APIs

--- frontend/models/Review.ts
+++ frontend/models/Review.ts
@@ -4,18 +4,23 @@ interface IReview extends Document {
 	productId: string;
 	userId: string;
 	rating: number;
-	comment: string;
+	comment?: string;
 	createdAt: Date;
 	updatedAt: Date;
 }
 
-const ReviewSchema: Schema = new Schema({
-	productId: { type: Schema.Types.ObjectId, ref: 'Products', required: true },
-	userId: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
-	rating: { type: Number, min: 1, max: 5, required: true },
-	comment: { type: String, required: false },
-	createdAt: { type: Date, default: Date.now },
-	updatedAt: { type: Date, default: Date.now }
-});
+const ReviewSchema: Schema = new Schema(
+	{
+		productId: { type: Schema.Types.ObjectId, ref: "Products", required: true },
+		userId: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+		rating: { type: Number, min: 1, max: 5, required: true },
+		comment: { type: String, maxlength: 500 },
+	},
+	{
+		timestamps: true,
+	}
+);
 
-export default (mongoose.models.reviews) || mongoose.model<IReview>('Reviews', ReviewSchema);
+ReviewSchema.index({ productId: 1, userId: 1 }, { unique: true });
+
+export default mongoose.models.Reviews || mongoose.model<IReview>("Reviews", ReviewSchema);
--- frontend/models/Whishlist.ts
+++ frontend/models/Whishlist.ts
@@ -1,17 +1,39 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document, model, models } from "mongoose";
 
-interface IWhishlist extends Document {
+interface IWishlist extends Document {
 	userId: string;
 	products: string[];
 	createdAt: Date;
 	updatedAt: Date;
 }
 
-const WhishlistSchema: Schema = new Schema({
-	userId: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
-	products: [{ type: Schema.Types.ObjectId, ref: 'Products' }],
-	createdAt: { type: Date, default: Date.now },
-	updatedAt: { type: Date, default: Date.now }
-});
+const WishlistSchema: Schema = new Schema(
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: "Users",
+			required: true,
+			unique: true,
+		},
+		products: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Products",
+				unqiue: true,
+			},
+		],
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-export default (mongoose.models.whishlists) || mongoose.model<IWhishlist>('Whishlist', WhishlistSchema);
+export default models.Wishlist as mongoose.Model<IWishlist> || model<IWishlist>("Wishlist", WishlistSchema);
