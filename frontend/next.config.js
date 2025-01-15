9f61a6310c0525d3afc3dd9cd2561b4ca41b5762
Vegadjay <jayvegad10@gmail.com>
2025-01-15 12:43:10 +0530

    Updatation Is Done

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    domains: [
+      "api.microlink.io",
+    ]
+  },
 };
 
 module.exports = nextConfig;
